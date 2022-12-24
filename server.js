9be0961e0fce4b92a7d1a9af62da18f991d0a47f
yd09 <66961735+erkushagra@users.noreply.github.com>
2022-12-24 14:35:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
 
     console.log(req.files)
     let d = await mergepdfs(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path))
-    res.redirect(`http://localhost:3000/static/${d}.pdf`)
+    res.redirect(`https://erkushagra.github.io/${d}.pdf`)
 
     // res.send({data:req.files})
     // req.files is array of `photos` files
